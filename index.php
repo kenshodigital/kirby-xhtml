deb341904d7da2de940204a426d3f35aed4743b9
Jens Scherbl <jens@scherbl.com>
2023-02-03 22:28:49 +0100

    Simplifies implementation

--- index.php
+++ index.php
@@ -1,15 +1,46 @@
-<?php declare(strict_types=1);
+<?php
+/**
+ * @noinspection PhpUnhandledExceptionInspection
+ */
+declare(strict_types=1);
 
 namespace Kenshō\XHTML;
 
 use DOMDocument;
 use Kirby\Cms\App;
 
+const XML = [
+    'htm',
+    'html',
+    'rss',
+    'xht',
+    'xhtml',
+    'xml',
+    'xsl',
+];
+const XHTML = [
+    'htm',
+    'html',
+    'xht',
+    'xhtml',
+];
 App::plugin('kensho/xhtml', [
     'hooks' => [
+        /**
+         * Ensures well-formed XML and XHTML
+         * output and strips whitespace between
+         * nodes.
+         */
         'page.render:after' => function (string $contentType, array $data, string $html): string {
-            if (\in_array($contentType, XMLProcessor::TYPES)) {
-                $html = (new XMLProcessor(new DOMDocument))->process($html);
+            if (\in_array($contentType, XML)) {
+                $document                     = new DOMDocument;
+                $document->preserveWhiteSpace = FALSE;
+                $document->loadXML($html);
+
+                if (\in_array($contentType, XHTML)) {
+                    App::instance()->response()->type('application/xhtml+xml');
+                }
+                return $document->saveXML();
             }
             return $html;
         },
